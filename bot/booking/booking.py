0efec39bc222f74fd90c66e8f6d54bc8a605378d
AliZaiN-157 <vin.alizain@gmail.com>
2021-05-18 21:22:27 +0500

    Updated the bot

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -9,6 +9,8 @@ class Booking(webdriver.Chrome):
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -16,3 +18,23 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        select_currency_element = self.find_element_by_css_selector(
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+        select_currency_element.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element_by_id('ss')
+        search_field.clear()
+        search_field.send_keys(place_to_go)
+
+        first_result = self.find_element_by_css_selector(
+            'li[data-i="0"]'
+        )
+        first_result.click()
--- bot/run.py
+++ bot/run.py
@@ -2,3 +2,5 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
+    bot.change_currency(currency='SAR')
+    bot.select_place_to_go("New York")
