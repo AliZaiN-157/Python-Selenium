8c9fe82ceb57297f418dfb130dea0f46852fc856
AliZaiN-157 <vin.alizain@gmail.com>
2021-05-18 20:19:36 +0500

    new bot created

--- checkgithub.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-driver.get(
-    "https://github.com/AliZaiN-157")
--- sendingkeys.py
+++ sendingkeys.py
@@ -1,6 +1,6 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
