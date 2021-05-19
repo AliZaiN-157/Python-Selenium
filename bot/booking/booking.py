f46175ec6c5bf28727b7d104318f0e556389a189
AliZaiN-157 <vin.alizain@gmail.com>
2021-05-20 02:34:21 +0500

    .gitignore added

--- bot/booking/booking.py
+++ bot/booking/booking.py
@@ -38,3 +38,45 @@ class Booking(webdriver.Chrome):
             'li[data-i="0"]'
         )
         first_result.click()
+
+    def select_date(self, check_in_date, check_out_date):
+        check_in_date = self.find_element_by_css_selector(
+            f'td[data-date="{check_in_date}"]'
+        )
+        check_in_date.click()
+        check_out_date = self.find_element_by_css_selector(
+            f'td[data-date="{check_out_date}"]'
+        )
+        check_out_date.click()
+
+    def select_adults(self, count):
+        selection_element = self.find_element_by_id('xp__guests__toggle')
+        selection_element.click()
+
+        while True:
+            decrease_adult_element = self.find_element_by_css_selector(
+                'button[aria-label="Decrease number of Adults"]'
+            )
+            decrease_adult_element.click()
+
+            '''If the value of adult reaches 1, 
+            then we should get out of the while loop '''
+
+            adults_value_element = self.find_element_by_id('group_adults')
+            adults_value = adults_value_element.get_attribute(
+                'value')  # Should give back the adult count
+
+            if int(adults_value) == 1:
+                break
+
+        increase_button_element = self.find_element_by_css_selector(
+            'button[aria-label="Increase number of Adults"]'
+        )
+
+        for _ in range(count - 1):
+            increase_button_element.click()
+
+    def click_search(self,):
+        search_button = self.find_element_by_css_selector(
+            'button[type="submit"]')
+        search_button.click()
--- bot/run.py
+++ bot/run.py
@@ -2,5 +2,9 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    bot.change_currency(currency='SAR')
+    # bot.change_currency(currency='SAR')
     bot.select_place_to_go("New York")
+    bot.select_date(check_in_date="2021-05-20",
+                    check_out_date="2021-05-29")
+    bot.select_adults(count=10)
+    bot.click_search()
